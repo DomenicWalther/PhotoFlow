791069d1450a8cdf9f66780a34db9bea6073c312
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2022-11-01 21:53:55 +0100

    Swapping over to Typescript

---
+++ src/app.d.ts
@@ -0,0 +1,22 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+// and what to do when importing types
+
+declare namespace Lucia {
+	type Auth = import("$lib/server/lucia").Auth;
+	type UserAttributes = {
+		username: string;
+	};
+}
+
+declare namespace App {
+	interface Locals {
+		getSession: import("@lucia-auth/sveltekit").GetSession;
+		setSession: import("@lucia-auth/sveltekit").SetSession;
+		clearSession: import("@lucia-auth/sveltekit").ClearSession;
+	}
+	// interface PageData {}
+	// interface Error {}
+	// interface Platform {}
+
+}
