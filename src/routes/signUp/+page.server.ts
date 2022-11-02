8b97b10528c730c547899ddd06d2ad19749184a8
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2022-11-03 00:43:40 +0100

    A

--- src/routes/signUp/+page.server.ts
+++ src/routes/signUp/+page.server.ts
@@ -1,11 +1,6 @@
 import { invalid, redirect, type Actions} from "@sveltejs/kit"
 import { auth } from "$lib/server/lucia"
-import { random, customRandom} from "nanoid"
 
-function generateCode() {
-    const characters = "1234567890";
-    return customRandom(characters, 8, random())();
-}
 export const actions: Actions = {
     default: async ({ request, locals }) => {
         const form = await request.formData();
@@ -25,12 +20,6 @@ export const actions: Actions = {
                 }
             });
             const session = await auth.createSession(user.userId);
-            const code = generateCode()
-
-            await setUserVerificationCode(userId, {
-                code,
-                expires: new Date().getTime() + 1000 * 60 * 24 * 8
-            });
             locals.setSession(session)
         } catch {
             return invalid(400)
