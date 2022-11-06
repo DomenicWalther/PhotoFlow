d26b2270d07482ed79ce96cfcb573ccec7b87f53
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2022-11-06 21:32:46 +0100

    Feat: Components for QR-Code Generation

---
+++ src/lib/QRCode.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { onMount } from 'svelte';
+      
+      export let codeValue;
+      export let squareSize; 
+      
+    let qrcode;
+              
+      onMount(() => {
+  
+          let script = document.createElement('script');
+      script.src = "/qrcode.min.js"
+      document.head.append(script);
+      
+          script.onload = function() {
+  
+              qrcode = new QRCode("qrcode", {
+                  text: codeValue,
+                width: squareSize,
+          height: squareSize,
+          colorDark : "#000000",
+          colorLight : "#ffffff",
+          correctLevel : QRCode.CorrectLevel.H
+              });		
+              
+          };				
+        
+      });
+  </script>
+  
+  <style>
+    #qrcode {
+      width:20px;
+      height:200x;
+      margin-top:15px;
+    }
+  </style>
+  
+  <div id="qrcode"></div>
+  
+  
