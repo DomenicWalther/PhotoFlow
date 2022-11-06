875a9c02d826b6ca96338653b2e0c55e33d90b2d
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2022-11-06 19:52:04 +0100

    Feat: Added Time Display to comments. Changed padding on comments.

--- src/routes/tasks/[slug]/+page.svelte
+++ src/routes/tasks/[slug]/+page.svelte
@@ -74,8 +74,10 @@
 					<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 						<dt class="text-sm font-medium text-gray-500">
 							{comment.createdAt.toLocaleDateString('de-DE', dateOptions)}
+							{comment.createdAt.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' })}
+
 						</dt>
-						<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{comment.comment}</dd>
+						<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 px-20">{comment.comment}</dd>
 					</div>
 				{/each}
 			{/if}
