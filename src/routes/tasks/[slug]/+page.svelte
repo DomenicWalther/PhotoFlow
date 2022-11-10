f7438e767621fa865ed320e28b07dfc39578bd58
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2022-11-10 12:53:58 +0100

    fix: Move Task Rows into it's own component.

--- src/routes/tasks/[slug]/+page.svelte
+++ src/routes/tasks/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+	import TaskRow from './TaskRow.svelte';
 	import type { PageData } from './$types';
-	import { getUser } from '@lucia-auth/sveltekit/client';
 	import { invalidateAll } from '$app/navigation';
 
 	const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -19,7 +19,7 @@
 			})
 		}).then((response) => {
 			comment = '';
-      invalidateAll();
+			invalidateAll();
 		});
 	};
 </script>
@@ -33,51 +33,42 @@
 	</div>
 	<div class="border-t border-gray-200">
 		<dl>
-			<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-				<dt class="text-sm font-medium text-gray-500">Client Name</dt>
-				<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-					{data.tasks?.task.charAt(0).toUpperCase() + data.tasks?.task.slice(1)}
-				</dd>
-			</div>
-			<div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-				<dt class="text-sm font-medium text-gray-500">Category</dt>
-				<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-					{data.tasks?.additional_information}
-				</dd>
-			</div>
-			<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-				<dt class="text-sm font-medium text-gray-500">Status</dt>
-				<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.tasks?.status}</dd>
-			</div>
-			<div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-				<dt class="text-sm font-medium text-gray-500">Due At</dt>
-				<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-					{data.tasks?.dueAt.toLocaleDateString('de-DE', dateOptions)}
-				</dd>
-			</div>
+			<TaskRow
+				label="Client Name"
+				data={data.tasks?.task.charAt(0).toUpperCase() + data.tasks?.task.slice(1)}
+			/>
+			<TaskRow label="Category" data={data.tasks?.additional_information} />
+			<TaskRow label="Status" data={data.tasks?.status} />
+			<TaskRow label="Due At" data={data.tasks?.dueAt.toLocaleDateString('de-DE', dateOptions)} />
+
 			<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 				<div class="max-w-lg ">
 					<form on:submit|preventDefault={submitComment} class="w-full p-4">
-						<label class="block mb-2">
+						<label class="mb-2 block">
 							<span class="text-gray-600">Add a comment</span>
-							<textarea class="block w-full mt-1 rounded" rows="3" bind:value={comment} />
+							<textarea class="mt-1 block w-full rounded" rows="3" bind:value={comment} />
 						</label>
-						<button class="px-3 py-2 text-sm text-blue-100 bg-blue-600 rounded">Comment</button>
+						<button class="rounded bg-blue-600 px-3 py-2 text-sm text-blue-100">Comment</button>
 					</form>
 				</div>
 			</div>
+
 			<div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-				<dt class="mt-1 text-m text-gray-900">Comments</dt>
+				<dt class="text-m mt-1 text-gray-900">Activity</dt>
 			</div>
 			{#if data.comments}
 				{#each data.comments as comment}
 					<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 						<dt class="text-sm font-medium text-gray-500">
 							{comment.createdAt.toLocaleDateString('de-DE', dateOptions)}
-							{comment.createdAt.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' })}
-
+							{comment.createdAt.toLocaleTimeString('de-DE', {
+								hour: '2-digit',
+								minute: '2-digit'
+							})}
 						</dt>
-						<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 px-20">{comment.comment}</dd>
+						<dd class="mt-1 px-20 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+							{comment.comment}
+						</dd>
 					</div>
 				{/each}
 			{/if}
---
+++ src/routes/tasks/[slug]/TaskRow.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let label: String = '';
+	export let data;
+	console.log(data);
+</script>
+
+<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+	<dt class="text-sm font-medium text-gray-500">{label}</dt>
+	<dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+		{data}
+	</dd>
+</div>
