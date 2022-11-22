4be5adec69b009a85626e1a023af93018ef71256
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2022-11-22 23:05:48 +0100

    Fix: Removed Prisma from Dashboard:

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,38 +1,35 @@
 import type { PageServerLoad } from './$types';
-import prisma from "$lib/server/prisma"
 
 export const load: PageServerLoad = async ({ cookies, locals }) => {
-    const date30DaysBack = new Date(new Date().setDate(new Date().getDate() - 30)).toISOString();
-    const session= await locals.getSession();
-    console.log(session?.userId)
-    const clientCount = await prisma.tasks.count({
-        where: {
-            user_id: session?.userId,
-            is_finished: false
-        }
-    })
+	const date30DaysBack = new Date(new Date().setDate(new Date().getDate() - 30)).toISOString();
+	const session = await locals.getSession();
+	console.log(session?.userId);
+	const clientCount = await prisma.tasks.count({
+		where: {
+			user_id: session?.userId,
+			is_finished: false
+		}
+	});
 
-    const pastClientCount = await prisma.tasks.count({
-        where: {
-            user_id: session?.userId, 
-            createdAt: {
-                gte: date30DaysBack
-            }
-        }
-    })
+	const pastClientCount = await prisma.tasks.count({
+		where: {
+			user_id: session?.userId,
+			createdAt: {
+				gte: date30DaysBack
+			}
+		}
+	});
 
-    const totalFinishedTasks = await prisma.tasks.count({
-        where: {
-            user_id: session?.userId,
-            is_finished: true
-        }
-    })
+	const totalFinishedTasks = await prisma.tasks.count({
+		where: {
+			user_id: session?.userId,
+			is_finished: true
+		}
+	});
 
-    return {
-        clientCount,
-        pastClientCount,
-        totalFinishedTasks
-    }
-}
-
-
+	return {
+		clientCount,
+		pastClientCount,
+		totalFinishedTasks
+	};
+};
