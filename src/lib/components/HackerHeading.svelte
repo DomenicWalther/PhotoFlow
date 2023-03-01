eca99d74fbf174bc45cce68e9da5a72e1f708694
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2023-03-01 22:32:38 +0100

    chore: Removed unused .svelte file

---
+++ src/lib/components/HackerHeading.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let text;
+	const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	let interval = null;
+	function changeText(event) {
+		let iteration = 0;
+
+		clearInterval(interval);
+
+		interval = setInterval(() => {
+			event.target.innerText = event.target.innerText
+				.split('')
+				.map((letter, index) => {
+					if (index < iteration) {
+						return event.target.dataset.value[index];
+					}
+
+					return letters[Math.floor(Math.random() * 26)];
+				})
+				.join('');
+
+			if (iteration >= event.target.dataset.value.length) {
+				clearInterval(interval);
+			}
+
+			iteration += 1 / 3;
+		}, 30);
+	}
+</script>
+
+<h1 data-value={text} on:mouseover={changeText}>{text}</h1>
