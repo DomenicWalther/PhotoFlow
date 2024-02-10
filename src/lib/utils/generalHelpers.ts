df63d3696612935d33f6ba80715b11b43218d008
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-02-10 21:45:27 +0100

    chore: added comment to sort_by function to explain how it works

--- src/lib/utils/generalHelpers.ts
+++ src/lib/utils/generalHelpers.ts
@@ -1,3 +1,16 @@
+/**
+ * Returns a custom comparison function for sorting an array of objects by a specified field.
+ * @param {string} field - The field by which to sort the array of objects.
+ * @param {boolean|number} reverse - Optional. Indicates whether to sort in reverse order. Defaults to false (ascending order).
+ * @param {Function} primer - Optional. A function to transform each element's field value before comparison.
+ * @returns {Function} - A comparison function to be used with the Array.prototype.sort() method.
+ * 
+ * The returned comparison function compares two objects from the array based on the specified field.
+ * If a primer function is provided, it transforms the field values before comparison.
+ * Sorting order (ascending/descending) is determined by the 'reverse' parameter.
+
+ */
+
 export const sort_by = (field, reverse: Boolean | Number, primer: Function) => {
 	const key = primer
 		? function (x) {
