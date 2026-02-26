273ca9162481e1a1f09f0808cc3a5f7d33911050
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-26 04:24:07 +0100

    chore: upgrade to tailwind v4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.postcss';
+	import '../style.css';
 	import 'fluent-svelte/theme.css';
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,31 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from "@tailwindcss/vite"
 import { Server } from 'socket.io';
 
 const config = {
-	plugins: [
-		sveltekit(),
-		{
-			name: 'sveltekit-socketio',
-			configureServer(server) {
-				if (!server.httpServer) return;
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    {
+      name: 'sveltekit-socketio',
+      configureServer(server) {
+        if (!server.httpServer) return;
 
-				const io = new Server(server.httpServer, {
-					cors: {
-						origin: process.env.VITE_CORS_ORIGIN || '*',
-						methods: ['GET', 'POST']
-					}
-				});
+        const io = new Server(server.httpServer, {
+          cors: {
+            origin: process.env.VITE_CORS_ORIGIN || '*',
+            methods: ['GET', 'POST']
+          }
+        });
 
-				io.on('connection', (socket) => {
-					socket.on('database-change', (msg) => {
-						io.emit('database-changed', msg);
-					});
-				});
-			}
-		}
-	]
+        io.on('connection', (socket) => {
+          socket.on('database-change', (msg) => {
+            io.emit('database-changed', msg);
+          });
+        });
+      }
+    }
+  ]
 };
 
 export default config;
